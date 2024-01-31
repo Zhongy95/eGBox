179e701002a862a0bd63820f2b705ae5ff997e7f
Zhongy95 <51180930+Zhongy95@users.noreply.github.com>
2024-01-31 12:11:34 +0800

    Update bindings.h

--- bindings.h
+++ bindings.h
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-2
 //
-// Copyright (C) 2021  William Findlay
-//
-// Jan. 19, 2021  William Findlay  Created this.
 
-// TODO: Include your bindings here
+
+// Include your bindings here
 
 #include "src/bpf/include/policy.h"
